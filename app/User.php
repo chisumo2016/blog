a803c0f133eb79ba8e9d41176af54500ab017388
Bernard Chisumo <bchisumo74@yahoo.com>
2016-12-13 11:46:27 +0000

    admin master page setup

--- app/User.php
+++ app/User.php
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // Setup Relationship
+    public function  role()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,1 @@
+<h1>It Working</h1>
