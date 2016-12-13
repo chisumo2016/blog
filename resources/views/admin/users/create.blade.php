2aa30cc397fd2aef998d08dc4a5f6b46472b3c43
Bernard Chisumo <bchisumo74@yahoo.com>
2016-12-13 16:23:00 +0000

    users with uploading features working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -27,9 +27,9 @@
                {!! Form::label('is_active', 'Status')  !!}
                 {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), 0 , ['class'=>'form-control']) !!}
            </div>
-            <div class="form-grop">
-                {!! Form::label('file', 'Title')  !!}
-                 {!! Form::file('file', null, ['class'=>'form-control']) !!}
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo')  !!}
+                 {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
             </div>
            <div class="form-group">
                {!! Form::label('password', 'Password')  !!}
