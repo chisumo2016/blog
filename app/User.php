8b46d89c843b7af16a914349ee3951e1c2ffc324
Bernard Chisumo <bchisumo74@yahoo.com>
2016-12-21 12:07:46 +0000

    The Blog  Appplication is working According .This Application was developed with Laravel 5.2

--- app/User.php
+++ app/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable
 
     public  function getGravatarAttribute()
     {
-        $hash = md5(strtolower(trim($this->attributes['email'])));
+        $hash = md5(strtolower(trim($this->attributes['email']))) . "?d=mm&s=";
         return "http://www.gravatar.com/avatar/$hash";
     }
 }
